85578b1612615e4c8d65f1f4be5fab00520d004b
Ruslan Simeonov <ruslansimenov@gmail.com>
2025-02-08 18:20:38 +0100

    update next config for deploy

--- frontend-app/next.config.ts
+++ frontend-app/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true, // ✅ Enables React strict mode (recommended)
+  swcMinify: true, // ✅ Enables SWC for faster builds
+  experimental: {}, // ✅ Empty, since `appDir` is no longer needed here
 };
 
 export default nextConfig;
