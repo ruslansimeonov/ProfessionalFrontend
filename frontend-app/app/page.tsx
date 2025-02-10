13befe6cc047e78c9af50772e579690cbd671437
Ruslan Simeonov <ruslansimenov@gmail.com>
2025-02-10 15:28:23 +0100

    update env

--- frontend-app/app/page.tsx
+++ frontend-app/app/page.tsx
@@ -12,6 +12,7 @@ export default function Home() {
     email: string;
   } | null>(null);
   const [loading, setLoading] = useState(true);
+  
 
   useEffect(() => {
     fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/hello`)
--- frontend-app/next.config.ts
+++ frontend-app/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },
 };
 
 export default nextConfig;
